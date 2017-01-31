d62b7f751ee21e88e83e9341c99d9e8f2d1387ef
Andrew Cuthbert <acuthbert@gcdtech.com>
2017-01-31 09:58:28 +0000

    Added ssl options

--- src/Settings/AwsSettings.php
+++ src/Settings/AwsSettings.php
@@ -3,6 +3,7 @@
 namespace Gcdtech\Aws\Settings;
 
 use Aws\Credentials\CredentialProvider;
+use Rhubarb\Crown\Exceptions\SettingMissingException;
 use Rhubarb\Crown\Settings;
 
 class AwsSettings extends Settings
@@ -12,12 +13,34 @@ class AwsSettings extends Settings
 
     public $iniCredentialsFile = "";
 
-    public function getClientSettings($additionalSettings)
+    /**
+     * Provide a path to a custom CA bundle if required
+     *
+     * http://docs.aws.amazon.com/aws-sdk-php/v3/guide/guide/configuration.html#verify
+     *
+     * @var string
+     */
+    public $sslCaBundlePemPath = "";
+
+    /**
+     * Set to false to disable SSL verification
+     * @var bool
+     */
+    public $sslVerify = true;
+
+    public function getClientSettings($additionalSettings = [])
     {
+        if (!$this->region){
+            throw new SettingMissingException("AwsSettings", "region");
+        }
+
         $settings = [
             'version' => 'latest',
             'region' => $this->region,
-            'profile' => $this->profile
+            'profile' => $this->profile,
+            'http'    => [
+                'verify' => ($this->sslVerify ? ($this->sslCaBundlePemPath ? $this->sslCaBundlePemPath : true) : false)
+            ]
         ];
 
         if ($this->iniCredentialsFile){
